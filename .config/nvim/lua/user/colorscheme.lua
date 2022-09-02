9202d577eb6d6a6decf74df1c16ab56e4bfaf9eb
ethanhuang <ethanhuang0606@gmail.com>
2022-09-03 01:59:15 +0800

    add lualine

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -4,7 +4,8 @@
 
 -- local colorscheme = "tokyonight"
 --[[ local colorscheme = "gruvbox" ]]
-local colorscheme = "nightfox"
+--[[ local colorscheme = "nightfox" ]]
+local colorscheme = "dracula"
 
 -- protected call
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -166,6 +166,9 @@ return packer.startup(function(use)
 		end,
 	})
 
+  -- status line
+	use("nvim-lualine/lualine.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
