8438f0bea1f10febc6b116adffc566fb18f1277e
EthanHuang <ethanhuang0606@gmail.com>
2026-01-14 23:39:39 -0800

    make lazygit float window bigger

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -14,4 +14,8 @@ return {
   keys = {
     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
+  init = function()
+    -- Floating window size (0.0 to 1.0 = percentage of screen)
+    vim.g.lazygit_floating_window_scaling_factor = 1.0
+  end,
 }
